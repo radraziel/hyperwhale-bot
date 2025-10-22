d4002c32cb900d8aea7cfe42926744a40e71376f
radraziel <79176084+radraziel@users.noreply.github.com>
2025-10-22 11:38:39 -0600

    main.py

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2865
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+from keep_alive import keep_alive
+from hyper_alerts import run_bot
 
-\f0\fs24 \cf0 from keep_alive import keep_alive\
-from hyper_alerts import run_bot\
-\
-if __name__ == "__main__":\
-    # levanta un mini-servidor web para que UptimeRobot mantenga vivo el Repl\
-    keep_alive()\
-    # arranca el loop del bot\
-    run_bot()}
+if __name__ == "__main__":
+    keep_alive()
+    run_bot()
